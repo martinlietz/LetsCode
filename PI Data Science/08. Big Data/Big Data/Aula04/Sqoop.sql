b4725f938231d5f812b6f539c1e9c58400e6d525
Martin Lietz <flieger.martin@web.de>
2022-03-16 22:53:12 -0300

    add resto

---
+++ PI Data Science/08. Big Data/Big Data/Aula04/Sqoop.sql
@@ -0,0 +1,20 @@
+--Em um terminal, acesse o mysql pelo comando: 
+mysql -uroot -pcloudera 
+
+--Listar databases: 
+SHOW databases;
+
+--Criar um Database
+CREATE DATABASE letscode;
+
+--Selecionar Database
+USE letscode;
+
+--Listar tabelas de um database: 
+SHOW TABLE from retail_db;
+
+--CRIAR TABELA
+CREATE TABLE cliente(id int, nome varchar(100));
+
+--inserir registros teste
+INSERT INTO cliente VALUES ((1, Maria),(2, Joao));
